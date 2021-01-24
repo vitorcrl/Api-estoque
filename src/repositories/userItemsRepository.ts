a0fd2080c70125ec1b4e10f6c38fa70be9a163ea
vitorcrl <vitor.carlos_souza@hotmail.com>
2021-01-24 04:54:09 -0300

    clean code

--- src/repositories/userItemsRepository.ts
+++ src/repositories/userItemsRepository.ts
@@ -1,10 +1,8 @@
 import { EntityRepository, Repository } from 'typeorm';
 
 import Item from '../models/Items';
-import User from '../models/Users';
-// responsavel por fazer as alteracoes no Banco de dados
 
-@EntityRepository(Item) // extende o repositorio e passa o parametros
+@EntityRepository(Item)
 class UserItemsRepository extends Repository<Item> {
   public async findItems(user: string): Promise<Item> {
     const findItems = await this.findItems(user);
--- src/routes/users.items.routes.ts
+++ src/routes/users.items.routes.ts
@@ -6,7 +6,6 @@ const usersItemsRouter = Router();
 
 usersItemsRouter.get('/', async (request, response) => {
   const { user } = request.body;
-  console.log(request.body);
   const showUser = getCustomRepository(UserItemsRepository);
   const users = await showUser.find({ where: { user } });
   return response.json(users);
